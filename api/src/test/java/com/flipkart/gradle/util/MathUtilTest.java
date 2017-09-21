51c82b5501c29af170209e0941fcf1a098f715bc
Imteyaz.A.Khan <khan.imteyaz@gmail.com>
2017-09-22 03:25:13 +0530

    sample_gradle_project

--- api/src/test/java/com/flipkart/gradle/util/MathUtilTest.java
+++ api/src/test/java/com/flipkart/gradle/util/MathUtilTest.java
@@ -2,13 +2,17 @@ package com.flipkart.gradle.util;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * Created by imteyaz.khan on 22/09/17.
  */
+@RunWith(JUnit4.class)
 public class MathUtilTest {
     @Test
     public void testSum() {
+        System.out.println("hahahahah");
        Assert.assertEquals(6, MathUtil.add(4, 2));
     }
 
